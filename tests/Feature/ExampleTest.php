935d79102b2c7557dacfe995fb676b7d4665ea4c
watanabe-m-e-ize <m.watanabe@e-ize.jp>
2020-11-27 11:17:07 +0000

    edit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,33 +1,21 @@
 <?php
 
- namespace Tests\Feature;
- use Illuminate\Foundation\Testing\RefreshDatabase;
- use Illuminate\Foundation\Testing\WithFaker;
- use Tests\TestCase;
+namespace Tests\Feature;
 
- class ExampleTest extends TestCase
- {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    use RefreshDatabase;
-    
-    /**
-     * @test
-     */
-    public function ユーザー登録できる()
-    {
-        $email = 'email@example.com';
-        $this->post(route('register'), [
-            'name' => 'user',
-            'email' => $email,
-            'password' => 'password',
-            'password_confirmation' => 'password'
-        ])
-            ->assertStatus(302);
-        $this->assertDatabaseHas('users', ['email' => $email]);
-    }
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
 }
- 
