d12e730e027e179ad5416bc4079a9a64707a0016
watanabe-m-e-ize <m.watanabe@e-ize.jp>
2020-11-27 11:11:30 +0000

    first commit

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Report;
+use Faker\Generator as Faker;
+
+$factory->define(Report::class, function (Faker $faker) {
+    return [
+        'visit_date' => now(),
+        'detail'     => $faker->text,
+    ];
+});
---
+++ tests/Feature/CustomerControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CustomerControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+    
+    /**
+     * @test
+     */
+    public function ユーザー登録できる()
+    {
+        $email = 'email@example.com';
+        $this->post(route('register'), [
+            'name' => 'user',
+            'email' => $email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseHas('users', ['email' => $email]);
+    }
+}
